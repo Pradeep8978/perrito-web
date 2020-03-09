853313fa8823f640638de25131e0be804e0a0d32
IndrajaRanga <59432993+IndrajaRanga@users.noreply.github.com>
2020-03-09 13:24:57 +0530

    route method changes

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -68,7 +68,6 @@ module.exports = {
     });
   },
   getAllOrders:(req,res)=>{
-    console.log("success");
     Orders.find({},function(err,allOrdersList){
       if(err){
         res.status(400).send(err);
@@ -78,5 +77,5 @@ module.exports = {
         res.status(200).json(allOrdersList);
       }
     })
-  }
-};
+  },
+  };
